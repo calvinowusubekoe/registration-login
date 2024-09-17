53417c71f011ef8637054d151df9766f7d4a26b9
Calvin Owusu Bekoe <calvinbekoe7@gmail.com>
2024-09-17 15:48:25 +0000

    added form data

--- scripts/register.js
+++ scripts/register.js
@@ -1,7 +1,24 @@
 // Get form element by id
-const registerForm = document.getElementById('register');
+const registerForm = document.querySelector('#register');
+console.log(registerForm);
 // Handle form submit event
-registerForm.onsubmit = function(event) {
+registerForm.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log(event);
-}
+    // Collect form data
+    const formData = new FormData(registerForm);
+    // Send Data to backend
+    // Display success message to user
+    const messageH1 = document.querySelector('#message');
+    messageH1.textContent = 'Account registered successfully!';
+    console.log(messageH1)
+});
+
+// Declare variable age
+let age = 71;
+// define square age function
+function squareAge (age) {
+    return age ** 2;
+} 
+
+const squaredAge = squareAge(34);
+console.log(squaredAge);
